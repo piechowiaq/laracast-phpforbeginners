6fb895f767a45583a8b8bd8cd4dc36d6bb40603d
Bartosz Piechowiak <piechowiaq@gmail.com>
2022-12-18 18:40:48 +0100

    Autoloading and Extraction

--- controllers/notes/create.php
+++ controllers/notes/create.php
@@ -1,14 +1,14 @@
 <?php
 
-require 'Validator.php';
+require base_path("Core/Validator.php");
 
-$config = require 'config.php';
+$config = require base_path('config.php');
 $db = new Database($config['database']);
 
-$heading = 'Create Note';
+$errors = [];
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $errors = [];
+
 
     if (! Validator::string($_POST['body'], 1, 1000))
     {
@@ -18,9 +18,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (empty($errors)) {
         $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
             'body' => $_POST['body'],
-            'user_id' => 1
+            'user_id' => 6
         ]);
     }
 }
 
-require 'views/notes/create.view.php';
+view("notes/create.view.php", [
+    'heading' => 'Create Note',
+    'errors' => $errors
+]);
